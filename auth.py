675e6e10632df61dd402dc03cb23403efd52e50a
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-05-25 11:54:56 +0800

    Hack for no auth

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ from fastapi import Request, HTTPException
 def get_user_from_easy_auth(request: Request):
     header = request.headers.get("X-MS-CLIENT-PRINCIPAL")
     if not header:
-        raise HTTPException(status_code=401, detail="No authentication header")
+        return None
 
     decoded = base64.b64decode(header)
     decoded_json = json.loads(decoded)
