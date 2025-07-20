a797631259a8e82b349847e5a2da4e4d8f605e17
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-07-20 17:08:21 +0800

    Utils: download to .download directory

--- sandbox/file_downloader.py
+++ sandbox/file_downloader.py
@@ -7,10 +7,12 @@ from storage_account import AzureStorageAccount
 
 def main():
     storage = AzureStorageAccount()
+    download_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".download")
+    os.makedirs(download_dir, exist_ok=True)
     storage.get_file(
         container_name="knowledgestore",
         blob_name="cv/Jia Yu Lee_CV.pdf",
-        download_path="cv.pdf"
+        download_path=os.path.join(download_dir, "cv.pdf")
     )
 
 if __name__ == "__main__":
