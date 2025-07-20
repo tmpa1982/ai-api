5c1d75c0e0a1b6825a78ba09da8681dbf11c65dd
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-07-20 17:12:06 +0800

    Utils: download all files

--- sandbox/file_downloader.py
+++ sandbox/file_downloader.py
@@ -9,11 +9,17 @@ def main():
     storage = AzureStorageAccount()
     download_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".download")
     os.makedirs(download_dir, exist_ok=True)
-    storage.get_file(
-        container_name="knowledgestore",
-        blob_name="cv/Jia Yu Lee_CV.pdf",
-        download_path=os.path.join(download_dir, "cv.pdf")
-    )
+
+    blob_prefix = "cv"
+    blobs = storage.list_blobs(container_name="knowledgestore", prefix=blob_prefix)
+
+    for blob_name in blobs:
+        download_path = os.path.join(download_dir, os.path.basename(blob_name))
+        storage.get_file(
+            container_name="knowledgestore",
+            blob_name=blob_name,
+            download_path=download_path
+        )
 
 if __name__ == "__main__":
     main()
--- storage_account.py
+++ storage_account.py
@@ -15,3 +15,8 @@ class AzureStorageAccount:
             with open(download_path, "wb") as file:
                 file.write(blob_data)
         return blob_data
+
+    def list_blobs(self, container_name: str, prefix: Optional[str] = None):
+        container_client = self.blob_service_client.get_container_client(container_name)
+        blobs = container_client.list_blobs(name_starts_with=prefix)
+        return [blob.name for blob in blobs]
